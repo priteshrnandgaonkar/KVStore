0c2251a28bbaa095baed126ac531436814ed6c84
Pritesh Nandgaonkar <pncooldude91@gmail.com>
2017-02-08 10:44:25 +0530

    Initial Commit

---
+++ KVStore/KVStore.h
@@ -0,0 +1,19 @@
+//
+//  KVStore.h
+//  KVStore
+//
+//  Created by Pritesh Nandgaonkar on 8/2/17.
+//  Copyright © 2017 Pritesh Nandgaonkar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KVStore.
+FOUNDATION_EXPORT double KVStoreVersionNumber;
+
+//! Project version string for KVStore.
+FOUNDATION_EXPORT const unsigned char KVStoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KVStore/PublicHeader.h>
+
+
