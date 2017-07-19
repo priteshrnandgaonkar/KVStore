15b8c0ebf7d7e5d73bb7b4c54925367f690a6aaa
Pritesh Nandgaonkar <pncooldude91@gmail.com>
2017-07-19 18:05:37 +0530

    Add target info in package.swift

--- Package.swift
+++ Package.swift
@@ -4,5 +4,6 @@ import PackageDescription
 
 let package = Package(
     name: "KVStore",
+    targets: [Target(name:"KVStoreTests", dependencies: []), Target(name:"KVStore", dependencies:[])],
     dependencies: []
 )
